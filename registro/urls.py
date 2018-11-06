6950b4312a04b23e95dcb4305435a64f95426702
Patricio <patri.diazr@alumnos.duoc.cl>
2018-11-05 23:13:04 -0300

    add login

--- registro/urls.py
+++ registro/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path 
 from . import views
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponse
+#importar user
+from django.contrib.auth.models import User
+#sistema de autenticación 
+from django.contrib.auth import authenticate,logout, login as auth_login
+from django.contrib.auth.decorators import login_required
 
 urlpatterns =[
     path('',views.home,name="home"),
     path('registro/templates/Home.html',views.home,name="home"),
     path('registro/templates/contact.html',views.contact, name="contact"),
     path('registro/templates/Service.html',views.servise,name="service"),
-    path('registro/templates/tourism.html',views.tourism,name="tourism")
+    path('registro/templates/tourism.html',views.tourism,name="tourism"),
+    path('registro/templates/singup.html',views.singup, name="singup"),
+    path('registro/templates/login.html',views.login, name="login"),
+    path(r'^login/iniciar/$',views.login_iniciar,name="iniciar")
 ]
--- registro/views.py
+++ registro/views.py
@@ -10,3 +10,17 @@ def contact(request):
     return render(request,"contact.html")
 def tourism(request):
     return render(request,"tourism.html")
+def singup(request):
+    return render(request,"singup.html")
+def login(request):
+    return render(request,"login.html")
+
+def login_iniciar(request):
+    usuario = request.POST.get('dni','')
+    password = request.POST.get('password','')
+    user = authenticate(request,username=usuario, password=password)
+    if user is not None:
+        auth_login(request, user)
+        return HttpResponse('<script>alert("Inicio de sesión correcto."); window.location.href="/index/";</script>')
+    else:
+        return HttpResponse('<script>alert("Ocurrió un error, intenta nuevamente..."); window.location.href="/login/";</script>')
